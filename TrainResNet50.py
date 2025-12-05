e6bf42d7cf0ac7ad8a8f8cf41df76dddfad704d3
Pepe de Sintas Mesquida <josemaria.desintasmesquida@usp.ceu.es>
2025-12-05 11:47:22 +0100

    more

--- TrainResNet50.py
+++ TrainResNet50.py
@@ -5,13 +5,11 @@ from tensorflow.keras.applications.resnet50 import preprocess_input
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 import os
 
-# ---- CONFIG ----
 DATA_DIR = "/Users/pepedesintas/Desktop/TFG/all-mias/outputData"
 IMG_SIZE = (224, 224)
 BATCH_SIZE = 16
 EPOCHS = 20
 
-# ---- LOAD DATASETS ----
 train_ds = tf.keras.preprocessing.image_dataset_from_directory(
     os.path.join(DATA_DIR, "train"),
     labels="inferred",
@@ -44,24 +42,22 @@ train_ds = train_ds.cache().prefetch(AUTOTUNE)
 val_ds   = val_ds.cache().prefetch(AUTOTUNE)
 test_ds  = test_ds.cache().prefetch(AUTOTUNE)
 
-# ---- DATA AUGMENTATION ----
-data_augmentation = tf.keras.Sequential([
-    layers.RandomFlip("horizontal"),
-    layers.RandomRotation(0.05),
-    layers.RandomZoom(0.10),
-    layers.RandomContrast(0.15),
-])
+#* ---- DATA AUGMENTATION ----
+#data_augmentation = tf.keras.Sequential([
+#    layers.RandomFlip("horizontal"),
+#    layers.RandomRotation(0.05),
+#    layers.RandomZoom(0.10),
+#    layers.RandomContrast(0.15),
+#])
+#
 
-# ---- MODEL ----
 inputs = layers.Input(shape=(224,224,3))
-x = data_augmentation(inputs)
-x = preprocess_input(x)
+#x = data_augmentation(inputs)
+x = preprocess_input(inputs)
 
-# Load pretrained ResNet50 WITHOUT top layers
 base_model = ResNet50(include_top=False, weights="imagenet", input_tensor=x)
 base_model.trainable = False   # freeze before fine-tuning
 
-# Add classification head
 x = layers.GlobalAveragePooling2D()(base_model.output)
 x = layers.Dense(256, activation="relu")(x)
 x = layers.Dropout(0.4)(x)
@@ -77,13 +73,11 @@ model.compile(
 
 model.summary()
 
-# ---- CALLBACKS ----
 callbacks = [
     EarlyStopping(monitor="val_auc", patience=5, mode="max", restore_best_weights=True),
-    ModelCheckpoint("resnet50_mias.h5", monitor="val_auc", mode="max", save_best_only=True)
+    ModelCheckpoint("resnet50_mias3.h5", monitor="val_auc", mode="max", save_best_only=True)
 ]
 
-# ---- TRAIN ----
 history = model.fit(
     train_ds,
     validation_data=val_ds,
@@ -91,6 +85,5 @@ history = model.fit(
     callbacks=callbacks
 )
 
-# ---- EVALUATE ----
 print("Evaluating on test set:")
 model.evaluate(test_ds)
